11ac94f7cb323e87ec3ec2678b4a97f2ec31a184
mbordeaux <mbordeaux@amx1.io>
2024-12-17 11:42:06 +0100

    component button

---
+++ src/component/container/button/button.jsx
@@ -0,0 +1,7 @@
+export function Button (){
+
+
+  return(
+    <button>Je click sur moi</button>
+  )
+}
--- src/component/container/footer/footer.jsx
+++ src/component/container/footer/footer.jsx
@@ -1,10 +1,12 @@
 import './footer.css';
+import { Button } from '../button/button';
 export function Footer( ){
   let paragraphe = "FOOTER Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit necessitatibus, officiis ipsa accusamus ullam in illo unde velit deserunt, saepe, sunt consectetur fugiat laborum reprehenderit dolore natus. Facilis, in porro?"
   return (
    <footer>
     <p>Copyright 2025</p>
     <p>{paragraphe}</p>
+    <Button/>
    </footer>
   )
 }
