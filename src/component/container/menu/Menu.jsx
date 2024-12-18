2f430a5a80a16b30eefb086ceab381dc8b5f89aa
mbordeaux <mbordeaux@amx1.io>
2024-12-18 15:02:34 +0100

    Amelioration style du menu

--- src/component/container/menu/Menu.jsx
+++ src/component/container/menu/Menu.jsx
@@ -1,38 +1,47 @@
 import { Button } from "../button/Button";
 import { useState } from "react";
-export default function Menu (){
-
+export default function Menu() {
   const [hide, changeState] = useState(true);
-/**
- * 
- * true ==> cache le menu
- */
+  /**
+   *
+   * true ==> cache le menu
+   */
 
-function hideShow() {
-  changeState(!hide)
-  console.log('(:::::::::::::::', hide)
-}
+  function hideShow() {
+    changeState(!hide);
+  }
 
-
-  return(
+  return (
     <>
-    <div>
-    <Button
-    gereclick={hideShow}
-    color={"darkPink"}
-    >/\</Button>
-    <section className={hide ? "hidden" : ""}>
-      <h2>Menu</h2>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-      </ul>
-    </section>
-    </div>
-
+      <div className="flex flex-col items-center p-4 bg-gray-800 ">
+        <Button gereclick={hideShow} >
+          <span className="text-2xl">&#x25B2;</span>
+        </Button>
+        <section
+          className={`${
+            hide ? "hidden" : "block"
+          } transition-all duration-300 ease-in-out mt-6 bg-gray-500 shadow-xl rounded-lg p-6 w-64`}
+        >
+          <h2 className="text-xl font-semibold text-gray-800 mb-4">Menu</h2>
+          <ul className="space-y-2">
+            <li className="text-gray-700 hover:text-pink-600 cursor-pointer">
+              1
+            </li>
+            <li className="text-gray-700 hover:text-pink-600 cursor-pointer">
+              2
+            </li>
+            <li className="text-gray-700 hover:text-pink-600 cursor-pointer">
+              3
+            </li>
+            <li className="text-gray-700 hover:text-pink-600 cursor-pointer">
+              4
+            </li>
+            <li className="text-gray-700 hover:text-pink-600 cursor-pointer">
+              5
+            </li>
+          </ul>
+        </section>
+      </div>
     </>
-  )
+  );
 }
